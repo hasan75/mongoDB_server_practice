59f96e2a81375d8ff480f99e33db873c0a47a019
Hasan75 <hasanjab14@gmail.com>
2021-12-07 08:51:58 +0600

    nested req.params declaration learning

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.get('/users/:id', (req,res)=>{
     const user = users[id]
     res.send(user)
 })
+app.get('/fruits',(req,res)=>{
+    res.send(['Mangoes','banana','Apple'])
+})
+
+app.get('/fruits/mangoes/fazli',(req,res)=>{
+    res.send('Fazli Miya Tok Marka')
+})
 
 app.listen(port,()=>{
     console.log('Server is running on port: ',port);
