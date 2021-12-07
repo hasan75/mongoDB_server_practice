90b572386bfd3efd172fb1c1f8e3af56550a2dd0
Hasan75 <hasanjab14@gmail.com>
2021-12-07 08:44:32 +0600

    hardcoded req,res value found

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const port = process.env.PORT || 5000 ;
 app.get('/',(req,res)=>{
     res.send('Hello From Node')
 })
+const users = [
+    {id:1, name:'Shabana', phone: '01716233854'},
+    {id:2, name:'Kuddus', phone: '01716233854'},
+    {id:3, name:'Akkas', phone: '01716233854'},
+    {id:4, name:'Kamalika', phone: '01716233854'},
+]
+app.get('/users',(req,res)=>{
+    res.send(users)
+})
+app.get('/users/:id', (req,res)=>{
+    const id = req.params.id
+    const user = users[id]
+    res.send(user)
+})
 
 app.listen(port,()=>{
     console.log('Server is running on port: ',port);
